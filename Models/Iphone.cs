feefecd3275047412a4bcb1b31e0fa847a9b4d3c
Antonio Silva <eddie.silva_6@hotmail.com>
2023-11-03 12:43:43 -0300

    Desafio concluído

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    // Herança da classe "Smartphone"
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            // Construtor da classe base (Smartphone) é chamado com os parâmetros necessários.
+        }
+
+        // Método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} em um Iphone...");
+        }
     }
 }
